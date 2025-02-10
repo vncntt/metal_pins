1aad5559325e13c09004f9a98a22c5b0b4c0cedb
vncntt <provincent8412@gmail.com>
2025-02-10 00:26:31 -0800

    deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "esnext",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
+  base: "/metal_pins/",
 });
